a5f7dd363a57df54d19888702dfa209a085c9188
jasperhoward <jgechoward@gmail.com>
2022-08-01 14:48:23 +0100

    /api/articles/:article_id endpoint

--- __tests__/api.test.js
+++ __tests__/api.test.js
@@ -29,3 +29,60 @@ describe("3. GET /api/topics", () => {
       });
   });
 });
+
+describe("4. GET /api/articles/:article_id", () => {
+  test("should return  status 200", () => {
+    return request(app).get("/api/articles/1").expect(200);
+  });
+  test("should respond with an article obj with correct properties", () => {
+    return request(app)
+      .get("/api/articles/2")
+      .then(({ body }) => {
+        const objOfCorrectShape = {
+          author: expect.any(String),
+          title: expect.any(String),
+          article_id: expect.any(Number),
+          body: expect.any(String),
+          topic: expect.any(String),
+          created_at: expect.any(String), // is there a timestamp costructor i need to use?
+          votes: expect.any(Number),
+        };
+
+        // console.log(body.article);
+        expect(body.article[0]).toMatchObject(objOfCorrectShape);
+      });
+  });
+  test("should respond with object specific to specified id", () => {
+    return request(app)
+      .get("/api/articles/1")
+      .then(({ body }) => {
+        const objCorrect = {
+          author: "butter_bridge",
+          title: "Living in the shadow of a great man",
+          article_id: 1,
+          body: "I find this existence challenging",
+          topic: "mitch",
+          created_at: "2020-07-09T20:11:00.000Z",
+          votes: 100,
+        };
+
+        expect(body.article[0]).toMatchObject(objCorrect);
+      });
+  });
+  test("should respond with 400 invalid id when given invalid author_id", () => {
+    return request(app)
+      .get("/api/articles/help")
+      .expect(400)
+      .then(({ body }) => {
+        expect(body.msg).toBe("invalid id");
+      });
+  });
+  test("should respond with 404 not found when given valid but non existent author_id", () => {
+    return request(app)
+      .get("/api/articles/100")
+      .expect(404)
+      .then(({ body }) => {
+        expect(body.msg).toBe("not found");
+      });
+  });
+});
