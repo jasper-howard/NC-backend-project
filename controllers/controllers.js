b4ed25dcffc0a4485b3de2afa8765696e7518895
jasperhoward <jgechoward@gmail.com>
2022-08-02 10:47:06 +0100

    5. PATCH /api/articles/:article_id, plus helper error function

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,8 +1,14 @@
-const { selectTopics, selectArticleById } = require("../models/models");
+const {
+  selectTopics,
+  selectArticleById,
+  changeVotes,
+} = require("../models/models");
+
+exports.log = (arg) => console.log(arg);
 
 exports.getTopics = (req, res, next) => {
   selectTopics().then((topics) => {
-    res.status(200).send(topics); // change to {topics: topic}??
+    res.status(200).send({ topics }); // change to {topics: topic}??
   });
 };
 
@@ -15,4 +21,21 @@ exports.getArticleById = (req, res, next) => {
     .catch((err) => {
       next(err);
     });
+};
+
+exports.updateVotes = (req, res, next) => {
+  const { inc_votes: votes } = req.body;
+  const { article_id: id } = req.params;
+  const b = Object.keys(req.body);
+  if (b.includes("inc_votes") && b.length === 1) {
+    changeVotes(votes, id)
+      .then((article) => {
+        res.status(200).send({ article: article });
+      })
+      .catch((err) => {
+        next(err);
+      });
+  } else {
+    res.status(406).send({ msg: "bad request" });
+  }
 };
--- models/models.js
+++ models/models.js
@@ -1,4 +1,5 @@
 const db = require("../db/connection");
+const { checkIfExits } = require("./utils");
 
 //try using asyn
 exports.selectTopics = async () => {
@@ -9,15 +10,32 @@ exports.selectTopics = async () => {
 exports.selectArticleById = async (id) => {
   try {
     const { rows: article } = await db.query(
-      `SELECT *FROM articles 
+      `SELECT * FROM articles 
              WHERE article_id = $1;`,
       [id]
     );
     if (article.length === 0) {
-      return Promise.reject({ status: 404, msg: "not found" });
+      return Promise.reject({ custom: "not found" });
     }
     return article;
   } catch (error) {
-    throw error;
+    return Promise.reject(error);
+  }
+};
+
+exports.changeVotes = async (v, id) => {
+  try {
+    const { rows: article } = await db.query(
+      "UPDATE articles SET votes = votes + $1 WHERE article_id = $2 RETURNING *;",
+      [v, id]
+    );
+
+    // should i invoke this helper in the controller with promise all?
+    if (!article.length) {
+      await checkIfExits("articles", "article_id", id);
+    }
+    return article;
+  } catch (error) {
+    return Promise.reject(error);
   }
 };
