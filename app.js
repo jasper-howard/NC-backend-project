06224925f517e5469a5b20af5afc43eb37879b6d
jasperhoward <jgechoward@gmail.com>
2022-08-22 09:56:39 +0100

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const {
   getTopics,
   getArticleById,
@@ -13,6 +14,7 @@ const {
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.get("/api", getEndpoints);
 
